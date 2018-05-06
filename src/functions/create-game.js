5c2a4bc81b53685563c790a3a353382347682ccf
Grégoire Weber <gweber@clever-age.com>
2018-05-06 15:33:21 -0400

    Add missing await in create game

--- src/functions/create-game.js
+++ src/functions/create-game.js
@@ -5,8 +5,7 @@ const { createGame } = require('../dao/game');
 // noinspection JSUnusedGlobalSymbols
 module.exports.handler = async (event, context, callback) => {
   try {
-
-    const result = createGame(initialGame);
+    const result = await createGame(initialGame);
     await successHandler(callback)(result);
   } catch (err) {
     await serverErrorHandler(callback)(err);
