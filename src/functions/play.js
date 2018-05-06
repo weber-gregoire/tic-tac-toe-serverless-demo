0ed5c2e177004686da083621bd69fdd4c6e3590f
Grégoire Weber <gweber@clever-age.com>
2018-05-06 16:25:05 -0400

    Add handler for play function

--- src/functions/play.js
+++ src/functions/play.js
@@ -1,9 +1,12 @@
 const { successHandler, serverErrorHandler } = require('../utils/handlers-helper');
+const { play } = require('../services/tic-tac-toe');
 
 // noinspection JSUnusedGlobalSymbols
 module.exports.handler = async (event, context, callback) => {
   try {
-    const result = {};
+    const { gameId } = event.path;
+    const { playerSymbol, coordinates } = event.body;
+    const result = await play(gameId, playerSymbol, coordinates);
     await successHandler(callback)(result);
   } catch (err) {
     await serverErrorHandler(callback)(err);
